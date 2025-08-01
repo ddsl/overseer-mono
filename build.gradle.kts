9f5fd363391cda35a6fa01dae4f7d1d35bae10bc
ddsl <1448850+ddsl@users.noreply.github.com>
2025-08-01 15:04:10 +0800

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.4" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+group = "io.github.ddsl.overseermono"
+version = "1.0-SNAPSHOT"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    //testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+subprojects {
+
+    layout.buildDirectory = file("${rootProject.projectDir}/dist/apps/${project.name}")
+
+    repositories {
+        mavenCentral()
+    }
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = JavaVersion.VERSION_22.toString()
+        targetCompatibility = JavaVersion.VERSION_22.toString()
+    }
+}
+
+tasks.forEach { task ->
+    // Отключаем только задачи корневого проекта
+    if (task.project == rootProject) {
+        task.enabled = false
+    }
+}
+
+tasks.register<Delete>("cleanDist") {
+    delete("$rootDir/dist")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "overseer-mono"
+
+include("auth")
+project(":auth").projectDir = file("apps/auth")
+
