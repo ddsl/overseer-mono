a4141759346f72fa74dda6394ba027cd5da07b3c
ddsl <1448850+ddsl@users.noreply.github.com>
2026-01-19 11:47:53 +0800

    chore(auth): set jpa settings & entities path in app class

--- apps/auth/src/main/java/io/github/ddsl/overseermono/auth/Application.java
+++ apps/auth/src/main/java/io/github/ddsl/overseermono/auth/Application.java
@@ -2,8 +2,15 @@ package io.github.ddsl.overseermono.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "io.github.ddsl.overseermono.commonlib.repositories")
+@EntityScan("io.github.ddsl.overseermono.commonlib.entities")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
