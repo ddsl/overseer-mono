406f382ca08db356cdd5d83a28dc55f05bef7e43
ddsl <1448850+ddsl@users.noreply.github.com>
2026-01-19 11:17:26 +0800

    build(common-lib): add validation dep

--- libs/common-lib/build.gradle.kts
+++ libs/common-lib/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
